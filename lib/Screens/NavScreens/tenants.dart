d933b1e5e5c0bc52279f720db9040f747c45ec83
Praditya-7 <manandhar.praditya7@gmail.com>
2021-11-10 20:03:03 +0545

    tenat add changes screen

--- lib/Screens/NavScreens/tenants.dart
+++ lib/Screens/NavScreens/tenants.dart
@@ -1,57 +1,68 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
-class Tenants extends StatelessWidget {
+class Tenants extends StatefulWidget {
   const Tenants({Key? key}) : super(key: key);
 
   @override
+  State<Tenants> createState() => _TenantsState();
+}
+
+class _TenantsState extends State<Tenants> {
+  int tenantCount = 0;
+  @override
   Widget build(BuildContext context) {
-    int tenantCount = 0;
     TextEditingController searchValue = TextEditingController();
     return Scaffold(
-        backgroundColor: Color(0xffF4F4F4),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              height: 50.0,
-              child: TextField(
-                controller: searchValue,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Color(0xff09548C),
-                  ),
-                  labelText: "Search by Name",
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xff09548C)),
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+      backgroundColor: Color(0xffF4F4F4),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0),
+            height: 50.0,
+            child: TextField(
+              controller: searchValue,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Color(0xff09548C),
+                ),
+                labelText: "Search by Name",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xff09548C)),
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
               ),
             ),
-            tenantCount == 0
-                ? Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      color: Colors.white,
-                      child: Center(child: Text("No Tenants Available")),
-                    ),
-                  )
-                : Expanded(
-                    child: Container(
-                      child: Text("Tenants Available"),
-                    ),
+          ),
+          tenantCount == 0
+              ? Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Center(child: Text("No Tenants Available")),
                   ),
-            FloatingActionButton(
-                child: Icon(
-                  Icons.add,
-                  color: Colors.white,
+                )
+              : Expanded(
+                  child: Container(
+                    child: Text("Tenants Available"),
+                  ),
                 ),
-                backgroundColor: Color(0xff09548C),
-                onPressed: () {}),
-          ],
-        ));
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          backgroundColor: Color(0xff09548C),
+          onPressed: () {
+            setState(() {
+              tenantCount++;
+              print(tenantCount);
+            });
+          }),
+    );
   }
 }
