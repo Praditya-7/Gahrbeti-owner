0eb68225abf1175313394f5e5e339812b1b3831a
Praditya-7 <manandhar.praditya7@gmail.com>
2021-11-12 10:47:47 +0545

    Room details added

--- lib/Screens/NavScreens/rooms.dart
+++ lib/Screens/NavScreens/rooms.dart
@@ -1,13 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
+import '../room_detail.dart';
+
 class Rooms extends StatelessWidget {
   const Rooms({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Rooms"),
+      child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => RoomDetail()));
+          },
+          child: Text("Rooms")),
     );
   }
 }
